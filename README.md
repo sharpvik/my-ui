163c5e4bbb358f239d12d9c048ba472f7b336f8d
Viktor Rozenko <sharp.vik@gmail.com>
2019-01-09 16:20:19 +0000

    components+

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# MyUI ultimate UI collection 
+# MyUI ultimate UI collection
 
 I don't suggest anything but I think it is **hot**.
 
 ## Pick what you like and implement in your own code
 
-Each section is divided by few empty lines in the HTML files. Each section has separate SASS and JavaScript files as well. This is for you to easily slide through and find what you want. I mean usually going through the source code is kinda long and boring not just because often it takes quite a bit of time but because it is hard to find all the pieces of code you really need. 
+Each section is divided by few empty lines in the HTML files. Each section has separate SASS and JavaScript files as well. This is for you to easily slide through and find what you want. I mean usually going through the source code is kinda long and boring not just because often it takes quite a bit of time but because it is hard to find all the pieces of code you really need.
 
-Necessary functions are often stacked in one place with dozens of others. HTML and CSS (SASS) might be written in a weird style so that you can't understand what is what. 
+Necessary functions are often stacked in one place with dozens of others. HTML and CSS (SASS) might be written in a weird style so that you can't understand what is what.
 
 **NOT IN VICLAND!!!**
 
@@ -15,6 +15,3 @@ Necessary functions are often stacked in one place with dozens of others. HTML a
 Enjoy.
 
 #### From Victor and Russel (my imaginary God) with love.
-
-You could also visit my [website](http://felp.website/). 
-If you want to contact me, please use the form in the footer.
--- widgets/scripts/bottom_navigation.js
+++ widgets/scripts/bottom_navigation.js
@@ -1,32 +1,32 @@
 $(document).ready(function() {
-    
+
     // Highlight the clicked navigator
     $('.bottom_navigator').on('click', function() {
         $('.bottom_navigator').removeClass('active_navigator');
         $(this).addClass('active_navigator');
-        
-        // Define the content of the card on click 
+
+        // Define the content of the card on click
         var clickedNavigator = $(this).attr('id');
-        
+
         if (clickedNavigator === 'recents') {
-            $('#news_card_image').css('background-image', 'url(../images/ny.jpg)');
+            $('#news_card_image').css('background-image', 'url(images/ny.jpg)');
             $('#news_card_title').html('New York');
             $('#news_card_section').html('Travel');
             $('#news_card_article').html('City than most any other place on the planet. In a fascinating New York Times Op-ed titled “My Endless New York” by acclaimed British historian Tony Judt, who came to New York University by way of Oxford, New York is, “a city more at home in the world than in its home country.');
             $('#news_card_link').attr('href', 'https://en.wikipedia.org/wiki/New_York');
         } else if (clickedNavigator === 'favorites') {
-            $('#news_card_image').css('background-image', 'url(../images/tokyo.jpg)');
+            $('#news_card_image').css('background-image', 'url(images/tokyo.jpg)');
             $('#news_card_title').html('Tokyo');
             $('#news_card_section').html('Business');
             $('#news_card_article').html('I walked out of Tokyo Station, the big commuter hub in the city’s Chiyoda district, and turned to look up at its gorgeous brick facade. I was lost. I couldn’t find the Tokyo Station Hotel — a well-reviewed luxury property that was renovated in 2012. My mistake, it turned out, was leaving the train station at all.');
             $('#news_card_link').attr('href', 'https://en.wikipedia.org/wiki/Tokyo');
         } else {
-            $('#news_card_image').css('background-image', 'url(../images/budapest.jpg)');
+            $('#news_card_image').css('background-image', 'url(images/budapest.jpg)');
             $('#news_card_title').html('Budapest');
             $('#news_card_section').html('Leisure');
             $('#news_card_article').html("From August 30 to September 4, Budapest will become the center of the short film world as the 12th annual Budapest Short Film Festival (BuSho) unspools its 74 masterpieces selected from 1,038 entries. The festival is a showcase of developing young filmmakers from all over the world competing in three categories: fiction, animation and experimental.");
             $('#news_card_link').attr('href', 'https://en.wikipedia.org/wiki/Budapest');
         }
-    });    
-    
+    });
+
 });
